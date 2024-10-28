b3203ee4669bca6c3ba910666b66f3f14119621f
GarrettMcNaLang <112025126+GarrettMcNaLang@users.noreply.github.com>
2024-10-28 13:45:32 -0400

    committhis

--- Assets/Scripts/PlayerIcon.cs
+++ Assets/Scripts/PlayerIcon.cs
@@ -1,16 +1,53 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerIcon : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private int _playerHealth;
+
+    public int PlayerHealth
     {
-        
+        get { return _playerHealth; }
+
+        set { _playerHealth = value;
+
+            if (_playerHealth <= 0)
+                Debug.Log("Player died event");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public int HealthtoSet;
+
+    private bool _IsAlive;
+    public bool IsAlive
     {
-        
+        get { return _IsAlive; }
+
+        set {
+            _IsAlive = value;
+
+            if (!_IsAlive)
+                Debug.Log("Initiate GameOver event and restrict player controls");
+        }
+    }
+
+    void OnEnable()
+    {
+        PlayerHealth = HealthtoSet;
+
+        IsAlive = true;
+        Debug.LogFormat("Health is: {0}", PlayerHealth);
+        Debug.LogFormat("Player is alive {0}", IsAlive);
+    }
+
+    void OnDisable()
+    {
+        IsAlive = false;
+    }
+
+    public Vector2 DistributeLocation()
+    {
+        return this.transform.position;
     }
 }
